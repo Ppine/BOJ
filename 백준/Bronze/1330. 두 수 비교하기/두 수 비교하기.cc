40850d13e4a0592967abc929efb893ae3e64632e
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2024-12-08 15:20:25 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b) printf("<");
+    else if(a>b) printf(">");
+    else if(a==b) printf("==");
+    return 0;
+}

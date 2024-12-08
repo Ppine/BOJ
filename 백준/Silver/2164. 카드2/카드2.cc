7982f69779a75920b06a6c7ddae437270ac11b8d
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2024-12-08 15:23:38 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4044 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+queue <int> q;
+int main()
+{
+	int n,k;
+	scanf("%d",&n);
+	for(int i=1; i<=n; i++)
+	{
+		q.push(i);
+	}
+	while(q.size()!=1)
+	{
+		q.pop();
+		k=q.front();
+		q.pop();
+		q.push(k);	
+	}
+	printf("%d",q.front());
+	return 0;
+}

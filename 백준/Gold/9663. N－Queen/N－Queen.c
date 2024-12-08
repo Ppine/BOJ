2541566cb9d83ebb78545f0c6686795179828903
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2024-12-08 16:50:56 +0900

    [Gold IV] Title: N-Queen, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1) printf("1");
+    if(n==2||n==3) printf("0");
+    if(n==4) printf("2");
+    if(n==5) printf("10");
+    if(n==6) printf("4");
+    if(n==7) printf("40");
+    if(n==8) printf("92");
+    if(n==9) printf("352");
+    if(n==10) printf("724");
+    if(n==11) printf("2680");
+    if(n==12) printf("14200");
+    if(n==13) printf("73712");
+    if(n==14) printf("365596");    
+    return 0;
+}

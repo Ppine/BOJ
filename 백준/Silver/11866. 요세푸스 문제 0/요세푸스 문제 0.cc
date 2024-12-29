4854c85f8fcacd5cf6579bbc228e6b113c9bc7b7
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2024-12-29 15:35:42 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -2,23 +2,27 @@
 
 using namespace std;
 
-queue <int> q;
 int main()
 {
-	int n,k;
-	scanf("%d %d",&n,&k);
-	for(int i=1; i<=n; i++) q.push(i);
-	
-	printf("<");
-	while(q.size()>1)
-	{
-		for(int i=0; i<k-1; i++)
-		{
-			q.push(q.front());
-			q.pop();
-		}
-		printf("%d, ",q.front()); q.pop();
-	}
-	printf("%d>",q.front());
-	return 0;
+    int n,k;
+    scanf("%d %d",&n,&k);
+
+    queue <int> q;
+
+    for(int i=1; i<=n; i++) q.push(i);
+
+    printf("<");
+    while(q.size()>1)
+    {  
+         for(int i=0; i<k-1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        printf("%d, ",q.front()); 
+        q.pop();
+    }
+    printf("%d>",q.front());
+
+    return 0;
 }

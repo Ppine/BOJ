598904282c5c5e953a9dd3b8e8ed3b2969be1db1
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2025-01-19 15:30:30 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char word[1005];
+    scanf("%s",word);
+    int n;
+    scanf("%d",&n);
+    printf("%c",word[n-1]);
+
+    return 0;
+}

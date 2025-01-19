3ed86b4ae5e77e6f1d9b5628b0d0f7ebb134f308
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2025-01-19 23:40:43 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    int h,w,n;
+
+    for(int i=0; i<t; i++)
+    {
+        scanf("%d %d %d",&h,&w,&n);
+        if(n%h==0)printf("%d%02d\n",h,n/h);
+        else printf("%d%02d\n",n%h,n/h+1);
+    }
+
+
+    return 0;
+}

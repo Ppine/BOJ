e9f6ae6031b4f79568e01cd453a5a2d0e9e6e62a
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2025-06-19 15:35:15 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,sum=0;
+    for(int i=0; i<4; i++){
+        scanf("%d",&n);
+        sum+=n;
+    }
+    printf("%d\n%d",sum/60,sum%60);
+
+    return 0;
+}

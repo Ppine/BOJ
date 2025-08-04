7a9bf08cfc1c2e5a0f32e54744d96c63700a95b1
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2025-08-04 16:29:04 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n,s;
+  while(cin>>n>>s){
+    cout << s/(n+1) << "\n";
+  }
+  return 0;
+
+}

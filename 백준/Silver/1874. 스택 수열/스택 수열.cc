a6fd4c61c7919389d7988b937d4370813d7bd9fb
ParkYoungMin <60142941+Ppine@users.noreply.github.com>
2025-08-07 16:22:09 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2556 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,35 +1,52 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-stack <int> s;
-char ans[200005];
-int main()
-{
-	int n,k[100005],m=0,cnt=0;
-	scanf("%d",&n);
-	s.push(++m); ans[cnt]='+'; cnt++;
-	for(int i=0;i<n;i++)
-	{
-		scanf("%d",&k[i]);
-	}
-	
-	for(int i=0;i<n;)
-	{
-		if(!s.empty() && s.top()==k[i])
-		{
-			ans[cnt]='-'; cnt++; s.pop(); i++;
-		}
-		else if(m<n) 
-		{
-			s.push(++m); ans[cnt]='+'; cnt++; 
-			
-		}
-		else break;		
-	}
-	
-	if(!s.empty() || m<n) { printf("NO"); return 0; }
-	for(int i=0; i<cnt; i++) printf("%c\n",ans[i]);
-	
-	return 0;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  int chk[100001];
+  fill(chk,chk+100001,1);
+  stack<char> ans;
+  stack<char> temp;
+  cin >> n;
+  int k;
+  int p=0,t;
+  bool pos=true;
+  while(n--){
+    t=p;
+    cin >> k;
+    if(chk[k]==0) continue;
+    if(p<k){
+      while(t<k){
+        t++;
+        if(chk[t]==1) ans.push('+');
+      }
+      ans.push('-');
+      p=t;
+      chk[p]=0;
+    }
+    else if(p>k){
+      while(t>k){
+        t--;
+        if(chk[t]==1&&t==k) ans.push('-');
+        else if(chk[t]==1 && t!=k) pos=false;
+      }
+      p=t;
+      chk[p]=0;
+    }
+  }
+  if(pos){
+    while(!ans.empty()){
+      temp.push(ans.top());
+      ans.pop();
+    }
+    while(!temp.empty()){
+      cout << temp.top() <<'\n';
+      temp.pop();
+    }
+  }
+  else cout << "NO";
+  return 0;
 }

95cbde2f621f8c3b5052a1647decbbf1f8008f60
Ppine <60142941+Ppine@users.noreply.github.com>
2025-11-09 12:24:50 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n,m;
+  cin >> n>>m;
+  cout << (n*8+m*3)-28;
+  return 0;
+}

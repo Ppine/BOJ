bf4e40c0fb5894b4843f73f5b70d144a2362a8c4
Youngmin Park <60142941+Ppine@users.noreply.github.com>
2026-02-04 18:57:03 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+
+  cin >> n;
+
+  for(int i=n; i>0; i--){
+    for(int j=0; j<n-i; j++) cout << " ";
+    for(int j=i*2-1; j>0; j--) cout << "*";
+    cout << '\n';
+  }
+
+  
+  return 0;
+}
